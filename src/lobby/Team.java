bb6934d6bdd72afa89fee4b2f4f97e4051dbed3f
ZhijunLin7 <zlin1@cifpfbmoll.com>
2023-05-08 19:55:40 +0200

    Esqueleto y metodos

---
+++ src/lobby/Team.java
@@ -0,0 +1,6 @@
+package lobby;
+
+public enum Team {
+    Red,
+    Blue,
+}
---
+++ src/lobby/lobbyController/LobbyController.java
@@ -0,0 +1,56 @@
+package lobby.lobbyController;
+
+import lobby.Account;
+import lobby.MasterStatus;
+import lobby.lobbyModel.LobbyModel;
+import lobby.lobbyView.LobbyView;
+
+public class LobbyController {
+
+    // Atributos
+    private MasterStatus status;
+    private LobbyView lobbyView;
+    private LobbyModel lobbyModel;
+
+    // Constructor
+    public LobbyController(MasterStatus status, LobbyView lobbyView, LobbyModel lobbyModel) {
+        this.status = status;
+        this.lobbyView = lobbyView;
+        this.lobbyModel = lobbyModel;
+    }
+
+    // Metodos
+    public void addAccount(Account account) {
+        lobbyModel.addAccount(account);
+    }
+
+    public void removeAccount(Account account) {
+        lobbyModel.removeAccount(account);
+    }
+
+    // Getter y Setters
+    public MasterStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(MasterStatus status) {
+        this.status = status;
+    }
+
+    public LobbyView getLobbyView() {
+        return lobbyView;
+    }
+
+    public void setLobbyView(LobbyView lobbyView) {
+        this.lobbyView = lobbyView;
+    }
+
+    public LobbyModel getLobbyModel() {
+        return lobbyModel;
+    }
+
+    public void setLobbyModel(LobbyModel lobbyModel) {
+        this.lobbyModel = lobbyModel;
+    }
+
+}
