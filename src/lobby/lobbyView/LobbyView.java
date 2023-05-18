63507d9afb9f4b5e6116b108856df150e27cf842
abascunana <abascunanasanchez@cifpfbmoll.eu>
2023-05-18 12:47:33 +0200

    correccion y JSON

--- src/lobby/lobbyView/LobbyView.java
+++ src/lobby/lobbyView/LobbyView.java
@@ -6,8 +6,14 @@ import lobby.lobbyModel.GameRules;
 
 import javax.swing.*;
 import java.awt.*;
-
+/**
+* Frame for the Lobby
+* 
+* @author Antoni Xavier Bascunana Sanchez
+* 
+*/
 public class LobbyView extends JFrame {
+    
    private Viewer viewer;
     private LobbyController lobbyController;
 
@@ -40,12 +46,14 @@ public class LobbyView extends JFrame {
             }
         }
     }
-   
+   /** 
+     *Add Viewer components
+     */
     public void addComponents() {
         // Delete background and add the one sent from the visual dep
         JLabel background = new JLabel(new ImageIcon("src/lobby/gameAssets/2space.jpg"));
         background.setLayout(new BorderLayout());
-//Game Parameters
+        //Game Parameters
         JPanel centerPanel = new JPanel();
         centerPanel.setOpaque(false);
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
@@ -59,7 +67,7 @@ public class LobbyView extends JFrame {
 
         centerPanel.add(Box.createVerticalGlue());
         centerPanel.add(Box.createVerticalStrut(300));
-//number of players waiting in lobby
+        //number of players waiting in lobby
 
         background.add(viewer.numplayers, BorderLayout.WEST);
         background.add(viewer.gameState, BorderLayout.EAST);
