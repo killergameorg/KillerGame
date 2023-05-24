57e222f116c6a94e7b4e620d5597d50e14891447
Carlos Rubio <crubiomorell@cifpfbmoll.eu>
2023-05-24 18:03:38 +0200

    AssetsManager rework 240523

--- src/visual_package/AssetsManager.java
+++ src/visual_package/AssetsManager.java
@@ -6,18 +6,19 @@ import java.util.Objects;
 
 public class AssetsManager {
 	
-	public static BufferedImage spaceShipA;
-	public static BufferedImage spaceShipB;
-	public static BufferedImage thrust;
-	public static BufferedImage bullet;
-	public static BufferedImage meteor;
+	public BufferedImage spaceShipA;
+	public BufferedImage spaceShipB;
+	public BufferedImage thrust;
+	public BufferedImage bullet;
+	public BufferedImage meteor;
 	
 	// Animations
-	public static BufferedImage[] bulletExplosion = new BufferedImage[10];
-	public static BufferedImage[] spaceShipExplosion = new BufferedImage[10];
+	public BufferedImage[] bulletExplosionAnimation = new BufferedImage[10];
+	public BufferedImage[] spaceShipExplosionAnimation = new BufferedImage[10];
+	public BufferedImage[] laserSparkleAnimation = new BufferedImage[10];
 
 	//Tags
-	public static BufferedImage[] playerTag = new BufferedImage[10];
+	public BufferedImage[] playerTag = new BufferedImage[10];
 
 	public AssetsManager() {
 		loadAssets();
@@ -31,14 +32,17 @@ public class AssetsManager {
 		bullet = spriteLoader("/visual_package/img/assets/laser.png");
 		meteor = spriteLoader("/visual_package/img/assets/meteor.png");
 
-		for(int i = 0; i < bulletExplosion.length; i++)
-			bulletExplosion[i] = spriteLoader("/visual_package/img/animations/bulletExplosion/"+i+".png");
+		for(int i = 0; i < bulletExplosionAnimation.length; i++)
+			bulletExplosionAnimation[i] = spriteLoader("/visual_package/img/animations/bulletExplosion/"+i+".png");
 
-		for(int i = 0; i < spaceShipExplosion.length; i++)
-			spaceShipExplosion[i] = spriteLoader("/visual_package/img/animations/spaceShipExplosion/"+i+".png");
+		for(int i = 0; i < spaceShipExplosionAnimation.length; i++)
+			spaceShipExplosionAnimation[i] = spriteLoader("/visual_package/img/animations/spaceShipExplosion/"+i+".png");
 
 		for(int i = 0; i < playerTag.length; i++)
-			playerTag[i] = spriteLoader("/visual_package/img/assets/playerTags/"+i+".png");
+			playerTag[i] = spriteLoader("/visual_package/img/assets/playerTag/"+i+".png");
+
+		for(int i = 0; i < laserSparkleAnimation.length; i++)
+			laserSparkleAnimation[i] = spriteLoader("/visual_package/img/animations/laserSparkle/"+i+".png");
 
 	}
 
@@ -51,5 +55,39 @@ public class AssetsManager {
 		return null;
 	}
 
+	public BufferedImage getSpaceShipA() {
+		return spaceShipA;
+	}
 
+	public BufferedImage getSpaceShipB() {
+		return spaceShipB;
+	}
+
+	public BufferedImage getThrust() {
+		return thrust;
+	}
+
+	public BufferedImage getBullet() {
+		return bullet;
+	}
+
+	public BufferedImage getMeteor() {
+		return meteor;
+	}
+
+	public BufferedImage[] getBulletExplosionAnimation() {
+		return bulletExplosionAnimation;
+	}
+
+	public BufferedImage[] getSpaceShipExplosionAnimation() {
+		return spaceShipExplosionAnimation;
+	}
+
+	public BufferedImage[] getLaserSparkleAnimation() {
+		return laserSparkleAnimation;
+	}
+
+	public BufferedImage[] getPlayerTag() {
+		return playerTag;
+	}
 }
