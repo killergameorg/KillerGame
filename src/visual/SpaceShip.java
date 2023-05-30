0c6c5b4f4d5e315fc5fe94281d401df4c72029d4
carlosblancoj <cblancojaner@cifpfbmoll.eu>
2023-05-30 19:30:44 +0200

    Code review

--- src/visual/SpaceShip.java
+++ src/visual/SpaceShip.java
@@ -1,8 +1,16 @@
 package visual;
+
 public class SpaceShip extends DynamicVisualObject {
-    public SpaceShip(int id, BufferedImage skin, Position position, float life, int accountId, VisualGameModel vgm,int playerNumber,Animation deadAnim, Animation spawnAnim, double width, double height, double velocity, double angle, PhysicsEngine pEngine) {
-        super(id, skin, position, life, accountId, vgm, playerNumber, deadAnim, spawnAnim, width, height, velocity, angle, pEngine);
+
+    
+    public SpaceShip(int id, BufferedImage skin, Position position, float life, int accountId, VisualGameModel visualGameModel,
+            int playerNumber, Animation deadAnim, Animation spawnAnim, double width, double height, double velocity,
+            double angle) {
+        super(id, skin, position, life, accountId, visualGameModel, playerNumber, deadAnim, spawnAnim, width, height, velocity,
+                angle);
     }
-    @Override 
-    public void run(){}
+
+    @Override
+    public void run() {
+    }
 }
